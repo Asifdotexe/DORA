4862c3ed916544bb0d28a01d0dd47679e90c9675
Asif Sayyed <asifdotexe@gmail.com>
2024-10-02 22:43:34 +0530

    #5 created a testing module for univariate analysis

---
+++ tests/test_univariate_analysis.py
@@ -0,0 +1,29 @@
+import os
+import pandas as pd
+from src.univariate_analysis import univariate_analysis
+
+def test_univariate_analysis():
+    """This function tests the univariate analysis function
+    """
+    # Setup
+    df = pd.DataFrame({
+        'A': [1, 2, 3, 4],
+        'B': ['x', 'y', 'x', 'y']
+    })
+    output_dir = 'tests/output_test/'
+    if not os.path.exists(output_dir):
+        os.makedirs(f'{output_dir}/stats/')
+        os.makedirs(f'{output_dir}/charts/')
+
+    # Test
+    stats_file = univariate_analysis(df, output_dir)
+
+    # Assertions
+    assert os.path.exists(stats_file), "Stats file was not created"
+    assert os.path.exists(f"{output_dir}/charts/univariate_A.png"), "Univariate chart for column A not created"
+    assert os.path.exists(f"{output_dir}/charts/univariate_B.png"), "Univariate chart for column B not created"
+
+    # Teardown
+    os.remove(stats_file)
+    os.remove(f"{output_dir}/charts/univariate_A.png")
+    os.remove(f"{output_dir}/charts/univariate_B.png")
