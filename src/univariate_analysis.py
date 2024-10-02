c6d2c694b8a52cf33b95fdf30b9eb2883b501e45
Asif Sayyed <asifdotexe@gmail.com>
2024-10-02 22:38:50 +0530

    #5 added docstrings

--- src/univariate_analysis.py
+++ src/univariate_analysis.py
@@ -4,19 +4,27 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def univariate_analysis(df: pd.DataFrame, output_directory: str) -> str:
+    """ This function performs a univariate analysis on the given DataFrame.
+
+    :param df: The input DataFrame containing the data to be analyzed.
+    :param output_directory: The directory where the analysis results will be saved.
+
+    :returns: The path to the file containing the statistical summary of the DataFrame.
+    :rtype: str
+    """
     stats = df.describe(include=all)
     stats_file = f"{output_directory}/stats/univariate_analysis.txt"
     stats.to_string(open(stats_file, 'w'))
-    
-    for column in df.select_dtypes(include=['float64','in64','object']):
+
+    for column in df.select_dtypes(include=['float64','int64','object']):
         plt.figure(figsize=(10,6))
         if df[column].dtype == 'object':
             sns.countplot(data=df, y=column)
         else:
             sns.histplot(data=df, x=column, kde=True)
-            
+
         plt.title(f'Univariate Analysis of {column}')
         plt.savefig(f"{output_directory}/charts/univariate_{column}.png")
         plt.close()
-        
+
     return stats_file
