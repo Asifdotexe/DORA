89a1919a75292dda4821c7a3258d0399b3f1fd1a
Asif Sayyed <asifdotexe@gmail.com>
2024-10-03 12:29:31 +0530

    #5 added docstrings to both the functions

--- src/save_results.py
+++ src/save_results.py
@@ -3,11 +3,21 @@ from pptx.util import Inches
 import os
 
 def create_ppt(output_directory: str, presentation_path: str) -> None:
+    """This function creates a PowerPoint presentation from the given output directory.
+    It reads the statistical analysis and chart files from the specified directories,
+    adds them as slides in the presentation, and saves the final presentation at the specified path.
+
+    :param output_directory: The directory containing the statistical analysis and chart files.
+    :param presentation_path: The path where the final presentation will be saved.
+
+    :returns: The function does not return any value, but it saves the presentation at the specified path.
+    :rtype: None
+    """
     prs = Presentation()
-    
+
     stats_files = os.listdir(f"{output_directory}/stats/")
     charts_files = os.listdir(f"{output_directory}/charts/")
-    
+
     # Add a slide for statistics
     stats_slide_layout = prs.slide_layouts[5]
     for stats_file in stats_files:
@@ -21,7 +31,7 @@ def create_ppt(output_directory: str, presentation_path: str) -> None:
             Inches(8),
             Inches(5)
         ).text = stats_content
-        
+
     # add slides for charts
     img_slide_layout = prs.slide_layouts[5]
     for chart_file  in charts_files:
@@ -34,11 +44,20 @@ def create_ppt(output_directory: str, presentation_path: str) -> None:
             Inches(8),
             Inches(5),
         )
-    
+
     # save the PowerPoint
     prs.save(presentation_path)
     
-def save_results(output_directory):
+def save_results(output_directory: str) -> str:
+    """
+    This function creates a PowerPoint presentation from the given output directory.
+    It reads the statistical analysis and chart files from the specified directories,
+    adds them as slides in the presentation, and saves the final presentation at the specified path.
+
+    :param output_directory: The directory containing the statistical analysis and chart files.
+
+    :returns presentation_path: The path of the saved presentation file.
+    """
     presentation_path = f"{output_directory}/eda_presentation.pptx"
     create_ppt(output_directory, presentation_path)
     return presentation_path
