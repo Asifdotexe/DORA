0b3111b3a14e40ad6773f6c0691fe2ae47e37452
Asif Sayyed <asifdotexe@gmail.com>
2024-10-03 11:40:17 +0530

    #5 created the testing module for multivariate analysis

---
+++ tests/test_multivariate_analysis.py
@@ -0,0 +1,26 @@
+import os 
+import numpy as np
+import pandas as pd
+from src.multivariate_analysis import multivariate_analysis
+
+def test_multivariate_analysis():
+    # Setup
+    df = pd.DataFrame({
+        'A': [1, 2, 3, 4],
+        'B': [10, 20, 30, 40],
+        'C': [100,200,300,np.nan],
+        'D': ['Asif','is','testing','this']
+    })
+    
+    output_dir = 'tests/output_test/'
+    if not os.path.exists(output_dir):
+        os.makedirs(f'{output_dir}/charts/')
+        
+    # test
+    multivariate_analysis(df, output_dir)
+    
+    # assertions
+    assert os.path.exists(f"{output_dir}/charts/charts/correlation_matrix.png"), "Correlation matrix chart not created"
+    
+    # teardown
+    os.remove(f"{output_dir}/charts/correlation_matrix.png")
