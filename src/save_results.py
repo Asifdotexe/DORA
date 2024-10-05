69a25595b3988111d1861a8efe6b5885a5b610d7
Asif Sayyed <asifdotexe@gmail.com>
2024-10-05 20:09:52 +0530

    #7 fixed the issue of all content being dumped on a single page

--- src/save_results.py
+++ src/save_results.py
@@ -15,39 +15,40 @@ def create_ppt(output_directory: str, presentation_path: str) -> None:
     """
     prs = Presentation()
 
+    # Get statistics and charts file lists
     stats_files = os.listdir(f"{output_directory}/stats/")
     charts_files = os.listdir(f"{output_directory}/charts/")
 
-    # Add a slide for statistics
-    stats_slide_layout = prs.slide_layouts[5]
+    # Add a slide for each statistical analysis file
     for stats_file in stats_files:
-        slide = prs.slides.add_slide(stats_slide_layout)
+        slide_layout = prs.slide_layouts[1]  # Title and content layout
+        slide = prs.slides.add_slide(slide_layout)
         with open(f"{output_directory}/stats/{stats_file}") as file:
             stats_content = file.read()
         slide.shapes.title.text = f"Statistical Analysis - {stats_file}"
         slide.shapes.add_textbox(
             Inches(1),
-            Inches(1),
+            Inches(1.5),
             Inches(8),
             Inches(5)
         ).text = stats_content
 
-    # add slides for charts
-    img_slide_layout = prs.slide_layouts[5]
-    for chart_file  in charts_files:
-        slides = prs.slides.add_slide(img_slide_layout)
+    # Add a slide for each chart file
+    for chart_file in charts_files:
+        slide_layout = prs.slide_layouts[5]  # Title only layout
+        slide = prs.slides.add_slide(slide_layout)
         slide.shapes.title.text = f"Chart - {chart_file}"
         slide.shapes.add_picture(
             f"{output_directory}/charts/{chart_file}",
             Inches(1),
-            Inches(1),
+            Inches(1.5),
             Inches(8),
             Inches(5),
         )
 
-    # save the PowerPoint
+    # Save the PowerPoint presentation
     prs.save(presentation_path)
-    
+
 def save_results(output_directory: str) -> str:
     """
     This function creates a PowerPoint presentation from the given output directory.
@@ -56,7 +57,7 @@ def save_results(output_directory: str) -> str:
 
     :param output_directory: The directory containing the statistical analysis and chart files.
 
-    :returns presentation_path: The path of the saved presentation file.
+    :returns: presentation_path: The path of the saved presentation file.
     """
     presentation_path = f"{output_directory}/eda_presentation.pptx"
     create_ppt(output_directory, presentation_path)
