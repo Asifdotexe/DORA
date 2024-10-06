79de8f794a32260a6565c414816ab432ed8f7337
Asif Sayyed <asifdotexe@gmail.com>
2024-10-06 12:23:06 +0530

    #9 added tqdm to bivariate module

--- src/bivariate_analysis.py
+++ src/bivariate_analysis.py
@@ -1,26 +1,30 @@
 import os
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
 sns.set_style('whitegrid')
 
 def bivariate_analysis(df: pd.DataFrame, output_directory: str) -> None:
-    """ This function performs a bivariate analysis on the given DataFrame, df, by creating scatter plots for each pair of numerical columns.
-    
+    """ This function performs a bivariate analysis on the given DataFrame.
+
     :param df: The input DataFrame containing the data to be analyzed.
     :param output_directory: The directory where the analysis results will be saved.
-
-    :returns: Nothing. Plots and saves the scatterplot in the given directory
-    :rtype: None
     """
+    
+    # Ensure the necessary directories exist
     charts_dir = os.path.join(output_directory, 'charts')
     os.makedirs(charts_dir, exist_ok=True)
+
+    # Select numerical columns for pairplots and correlation analysis
+    num_columns = df.select_dtypes(include=['float64', 'int64']).columns
     
-    numerical_columns = df.select_dtypes(include=['float64', 'int64']).columns
-    for index, column_1 in enumerate(numerical_columns):
-        for column_2 in numerical_columns[index + 1:]:
-            plt.figure(figsize=(10,6))
-            sns.scatterplot(x=df[column_1], y=df[column_2])
-            plt.title(f"Bivariate Analysis of {column_1} vs {column_2}")
-            plt.savefig(f"{output_directory}/charts/bivariate_{column_1}_vs_{column_2}.png")
+    for i, column in tqdm(enumerate(num_columns), desc="Bivariate Analysis", ncols=100, unit="column", colour='#008000', total=len(num_columns)):
+        # For each column, pair with all remaining columns
+        for j, col2 in enumerate(num_columns[i + 1:]):
+            plt.figure(figsize=(10, 6))
+            sns.scatterplot(data=df, x=column, y=col2)
+            plt.title(f'Bivariate Analysis: {column} vs {col2}')
+            plt.savefig(f"{charts_dir}/bivariate_{column}_vs_{col2}.png")
             plt.close()
