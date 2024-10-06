af1e8e8a902a2f366c5cd575a1f0f1126d8c0344
Asif Sayyed <asifdotexe@gmail.com>
2024-10-06 11:38:51 +0530

    #9 added argparser support for CLI

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import argparse
 import pandas as pd
 from src.univariate_analysis import univariate_analysis
 from src.bivariate_analysis import bivariate_analysis
@@ -5,7 +6,13 @@ from src.multivariate_analysis import multivariate_analysis
 from src.save_results import save_results
 import os
 
-def main(input_file, output_dir, template_path= None):
+def main(input_file, output_dir, template_path=None):
+    """Main function to perform EDA and save results to the output directory.
+
+    :param input_file: Path to the input CSV file.
+    :param output_dir: Directory where stats and charts will be saved.
+    :param template_path: Optional path to the PowerPoint template.
+    """
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
         os.makedirs(f"{output_dir}/stats/")
@@ -23,9 +30,16 @@ def main(input_file, output_dir, template_path= None):
     save_results(output_dir, template_path)
 
 if __name__ == '__main__':
-    input_file = '../data/insurance.csv'  # Change this to your input file
-    output_dir = 'output/'
-    # template_path = '../presentation/EDA AUTOMATION.pptx'
-    main(input_file, output_dir, 
-        #  template_path
-        )
+    # Initialize parser
+    parser = argparse.ArgumentParser(description='Perform EDA and create a PowerPoint presentation.')
+
+    # Add arguments
+    parser.add_argument('input_file', type=str, help='Path to the input CSV file.')
+    parser.add_argument('output_dir', type=str, help='Directory where output will be saved.')
+    parser.add_argument('--template_path', type=str, default=None, help='Optional path to PowerPoint template.')
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Call main function with parsed arguments
+    main(args.input_file, args.output_dir, args.template_path)
