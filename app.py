68a14de25ebd067fb3efa012c87c89f6c08f18d4
Asif Sayyed <asifdotexe@gmail.com>
2026-01-19 19:36:01 +0530

    #35 added streamlit and made basic scaffolding

---
+++ app.py
@@ -0,0 +1,14 @@
+"""
+Module to house the streamlit logic
+"""
+
+import streamlit as st
+
+
+def main():
+    st.title("DORA")
+    st.write("This is a simple app to demonstrate the use of DORA.")
+
+
+if __name__ == "__main__":
+    main()
